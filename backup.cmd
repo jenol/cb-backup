efc19705c2fc24527de28af24c772db7a4bb2f09
Jeno Laszlo <jenol@users.noreply.github.com>
2023-02-15 13:08:10 +0100

    Update backup.cmd

--- backup.cmd
+++ backup.cmd
@@ -2,4 +2,4 @@ set server=%1
 set path=%2
 
 D:
-"D:\Program Files\Couchbase\Server\bin\cbtransfer.exe" -u Administrator -p agoda123* -b ycs -k ^NHA_ http://%server%:8091 "%path%"
+"D:\Program Files\Couchbase\Server\bin\cbtransfer.exe" -u Administrator -p yourpassw -b ycs -k ^NHA_ http://%server%:8091 "%path%"
